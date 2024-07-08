a66daa63244731fea1977d9a0a1cffeee3c28bbc
dangabrielle <hoangabrielle@gmail.com>
2024-07-07 23:13:54 -1000

    fix prisma config

--- server/db/prisma.js
+++ server/db/prisma.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient({
   datasources: {
@@ -8,4 +8,4 @@ const prisma = new PrismaClient({
   },
 });
 
-export default prisma;
+module.exports = prisma;
