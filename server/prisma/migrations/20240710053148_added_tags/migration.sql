89d7c8b1e03dcec9018957525ad19e46f44d3cbd
dangabrielle <hoangabrielle@gmail.com>
2024-07-09 20:46:12 -1000

    change prisma schema

--- server/prisma/migrations/20240710053148_added_tags/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[nodeID]` on the table `Data` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Data_nodeID_key" ON "Data"("nodeID");
