85d872c14c470acf613a08abd69815c34e787023
dangabrielle <hoangabrielle@gmail.com>
2024-07-30 00:38:19 -1000

    fix text size for md screens or larger

--- client/src/app/components/SensorData.tsx
+++ client/src/app/components/SensorData.tsx
@@ -130,7 +130,7 @@ const SensorData = ({ initialData }: Props) => {
 
   return (
     <>
-      <div className="z-50 flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-2 absolute md:w-full ml-10 mr-10 md:justify-evenly md:items-start bg-transparent pt-5 text-white">
+      <div className="z-50 flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-2 absolute md:w-11/12 ml-10 mr-10 md:justify-evenly md:items-start bg-transparent pt-5 text-white">
         <div className="flex flex-col md:flex-row items-center">
           <Link
             href="https://arl.hawaii.edu/"
@@ -152,24 +152,20 @@ const SensorData = ({ initialData }: Props) => {
               target="_blank"
               className="flex items-start hover:scale-105 "
             >
-              <h1 className="relative text-white z-50 bg-green-800 p-2 pr-3 pl-3 bg-opacity-70 hover:bg-emerald-700 rounded shadow-gray-700 shadow-md mt-2 md:mt-0 md:mr-auto ml-5 text-center text-xs md:text-base hover:scale-105">
+              <h1 className="relative text-white z-50 bg-green-800 p-2 pr-3 pl-3 bg-opacity-70 hover:bg-emerald-700 rounded-md shadow-gray-700 shadow-md mt-2 md:mt-0 md:mr-auto ml-5 text-center text-xs md:text-sm lg:text-base hover:scale-105">
                 <b>APPLIED RESEARCH LABORATORY</b> <br /> University of
                 Hawai&apos;i
               </h1>
             </Link>
-            <div className="relative text-white bg-green-800 p-2 mt-2 bg-opacity-70 hover:bg-emerald-700 rounded shadow-gray-700 shadow-md text-center ml-5 text-xs md:text-base hover:scale-105">
-              <Link
-                href="/about"
-                // target="_blank"
-                className=" hover:scale-105 "
-              >
+            <div className="relative text-white bg-green-800 p-2 mt-2 bg-opacity-70 hover:bg-emerald-700 rounded-md text-center ml-5 text-xs md:text-sm lg:text-base hover:scale-105">
+              <Link href="/about" className=" hover:scale-105 ">
                 PROJECT SUMMARY
               </Link>
             </div>
           </div>
         </div>
 
-        <p className="relative text-white z-50  bg-sky-700 p-2 pr-3 pl-3 hover:bg-sky-700 bg-opacity-70 hover:scale-105 rounded  md:ml-auto text-center text-xs md:text-base">
+        <p className="relative text-white z-50  bg-sky-700 p-2 pr-3 pl-3 hover:bg-sky-700 bg-opacity-70 hover:scale-105 rounded-md  md:ml-auto text-center text-xs md:text-sm lg:text-base">
           <b>Connection status:</b>
           {isConnected
             ? ` active -
@@ -180,7 +176,7 @@ const SensorData = ({ initialData }: Props) => {
 
         <button
           onClick={openSideBar}
-          className="relative text-white z-50  bg-sky-700 p-2 pr-3 pl-3  hover:bg-sky-700 bg-opacity-70 hover:scale-105 rounded active:bg-blue-400 md:ml-auto text-xs md:text-base"
+          className="relative text-white z-50  bg-sky-700 p-2 pr-3 pl-3  hover:bg-sky-700 bg-opacity-70 hover:scale-105 rounded-md active:bg-blue-400 md:ml-auto text-xs md:text-sm lg:text-base"
         >
           View Sensors ({sensorData.length})
         </button>
@@ -189,7 +185,7 @@ const SensorData = ({ initialData }: Props) => {
         <div className="m-auto items-start md:ml-auto">
           {user && (
             <div className="flex items-center ml-auto mr-10">
-              <div className="relative text-white z-50 bg-gray-400 p-1 pr-3 pl-3 bg-opacity-70 hover:bg-gray-400 hover:scale-105 mr-3 rounded text-xs md:text-base -mt-2.5">
+              <div className="relative text-white z-50 bg-gray-400 p-1 pr-3 pl-3 bg-opacity-70 hover:bg-gray-400 hover:scale-105 mr-3 rounded-md text-xs md:text-sm lg:text-base -mt-2.5">
                 <a href="/api/auth/logout">Logout</a>
               </div>
               <Image
