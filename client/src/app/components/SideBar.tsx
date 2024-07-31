743f517e02e3988520f2bb958ae45f7cf78a9af2
dangabrielle <hoangabrielle@gmail.com>
2024-07-30 17:24:28 -1000

    fix sidebar overlap avatar icon

--- client/src/app/components/SideBar.tsx
+++ client/src/app/components/SideBar.tsx
@@ -38,17 +38,21 @@ const SideBar = ({ sensorData, isOpen, closeBar }: Props) => {
         } `}
       >
         <div>
-          <div className="font-bold pt-2 pl-1 pr-2 mt-2">
-            <button onClick={closeBar} className="text-xl hover:scale-125">
-              x
+          <div className="pt-1 md:pt-4 pl-1 pr-1 mt-14 m-auto md:text-md lg:text-base text-xs">
+            <button
+              onClick={closeBar}
+              className="hover:scale-105 bg-gray-400 pl-2 pr-2 mb-1 bg-opacity-70 hover:bg-gray-400
+            rounded-md"
+            >
+              close
             </button>
           </div>
           <div>
-            <ul className="absolute flex-col justify-center 2xl:w-11/12  text-center ml-6">
+            <ul className="absolute flex-col justify-center 2xl:w-11/12 mt-2 text-center ml-6">
               {sensorData.map((data, index) => (
                 <li
                   key={index}
-                  className="mt-1 ml-5 mb-3 mr-3 p-4 bg-sky-800 bg-opacity-60 hover:scale-105 rounded-md shadow-md transform transition-transform duration-200 ease-in-out hover:bg-sky-800 active:scale-105 text-xs md:text-md lg:text-base"
+                  className="mt-1 mb-3 mr-3 p-4 bg-sky-800 bg-opacity-60 hover:scale-105 rounded-md shadow-md transform transition-transform duration-200 ease-in-out hover:bg-sky-800 active:scale-105 text-xs md:text-md lg:text-base"
                 >
                   <button
                     onClick={() => handleClick(data.latitude, data.longitude)}
