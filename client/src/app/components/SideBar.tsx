00bfe7db53199501c35b86345a715cdf2eb711af
dangabrielle <hoangabrielle@gmail.com>
2024-07-30 16:56:15 -1000

    update sidebar

--- client/src/app/components/SideBar.tsx
+++ client/src/app/components/SideBar.tsx
@@ -33,28 +33,35 @@ const SideBar = ({ sensorData, isOpen, closeBar }: Props) => {
   return (
     <>
       <div
-        className={`fixed top-0 right-0 h-full left-1/2 md:left-3/4 text-white transition-transform transform z-50 overflow-auto ${
+        className={`fixed top-0 right-0 h-full left-1/2 md:left-3/4  text-white transition-transform transform z-50 overflow-auto ${
           isOpen ? "translate-x-100" : "translate-x-full"
         } `}
       >
         <div>
-          <div>
-            <button onClick={closeBar} className="text-xl">
+          <div className="font-bold pt-2 pl-1 pr-2 mt-2">
+            <button onClick={closeBar} className="text-xl hover:scale-125">
               x
             </button>
           </div>
           <div>
-            <ul className="absolute flex-col justify-center items-center content-center ml-5 mr-5">
+            <ul className="absolute flex-col justify-center 2xl:w-11/12  text-center ml-6">
               {sensorData.map((data, index) => (
-                <li key={index}>
+                <li
+                  key={index}
+                  className="mt-1 ml-5 mb-3 mr-3 p-4 bg-sky-800 bg-opacity-60 hover:scale-105 rounded-md shadow-md transform transition-transform duration-200 ease-in-out hover:bg-sky-800 active:scale-105 text-xs md:text-md lg:text-base"
+                >
                   <button
-                    className="mb-3 p-4 bg-blue-400 rounded shadow-md transform transition-transform duration-200 ease-in-out hover:bg-blue-300 hover:scale-100 active:scale-105 text-xs md:text-base"
                     onClick={() => handleClick(data.latitude, data.longitude)}
                   >
-                    Node ID: {data.nodeID}, Temperature: {data.temp}, Humidity:{" "}
-                    {data.humidity}, Latitude: {data.latitude}, Longitude:{" "}
-                    {data.longitude}, Time: {data.time}, Battery: {data.battery}
-                    , Health: {data.health}
+                    <b>Node ID: </b>
+                    {data.nodeID} <br /> <b>Temperature: </b>
+                    {data.temp} <br />
+                    <b>Humidity:</b> {data.humidity} <br /> <b>Latitude:</b>
+                    {data.latitude} <br /> <b>Longitude:</b> {data.longitude}
+                    <br />
+                    <b>Time:</b> {data.time} <br /> <b>Battery:</b>{" "}
+                    {data.battery} <br />
+                    <b>Health:</b> {data.health}
                   </button>
                 </li>
               ))}
